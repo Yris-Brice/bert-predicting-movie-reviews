7b8040ca3fc07f2657f3ceae7242332bf2b8907a
Janek Schleicher <postkasten@janek-schleicher.de>
2019-05-05 18:56:42 +0200

    Data preprocessing

--- defaults.py
+++ defaults.py
@@ -3,10 +3,16 @@ import os
 
 import numpy as np
 
+BERT_MODEL_HUB: str = os.environ.get(
+    "BERT_MODEL_HUB", "https://tfhub.dev/google/bert_uncased_L-12_H-768_A-12/1"
+)
 DATA_DIR: str = os.environ.get("DATA_DIR", "data")
 DATASET_NAMES: List[str] = ["train", "test"]
 FORCE_DOWNLOAD: bool = bool(os.environ.get("FORCE_DOWNLOAD", False))
+MAX_SEQ_LENGTH: int = int(os.environ.get("MAX_SEQ_LENGTH", 128))
+SAMPLE_SIZE: int = 5_000
 SEED: Union[int, np.random.RandomState] = int(os.environ.get("SEED", 42))
+
 
 np.random.seed(seed=SEED)
 
---
+++ preprocessing.py
@@ -0,0 +1,72 @@
+from typing import Tuple
+
+from bert import run_classifier, tokenization
+from fastparquet import ParquetFile
+import pandas as pd
+import tensorflow as tf
+import tensorflow_hub as hub
+
+from defaults import (
+    BERT_MODEL_HUB,
+    DATASET_NAMES,
+    MAX_SEQ_LENGTH,
+    SAMPLE_SIZE,
+    data_filename,
+)
+
+
+DATA_COLUMN = "sentence"
+LABEL_COLUMN = "polarity"
+LABEL_LIST = [0, 1]
+
+
+def create_bert_input_example(row: pd.Series) -> run_classifier.InputExample:
+    return run_classifier.InputExample(
+        guid=None,  # Globally unique ID for bookkeeping, unused in this example
+        text_a=row[DATA_COLUMN],
+        text_b=None,
+        label=row[LABEL_COLUMN],
+    )
+
+
+def create_tokenizer_from_hub_module() -> tokenization.FullTokenizer:
+    """Get the vocab file and casing info from the Hub module."""
+    with tf.Graph().as_default():
+        bert_module = hub.Module(BERT_MODEL_HUB)
+        tokenization_info = bert_module(signature="tokenization_info", as_dict=True)
+        with tf.Session() as sess:
+            vocab_file, do_lower_case = sess.run(
+                [tokenization_info["vocab_file"], tokenization_info["do_lower_case"]]
+            )
+    return tokenization.FullTokenizer(
+        vocab_file=vocab_file, do_lower_case=do_lower_case
+    )
+
+
+def create_train_test_features(
+    tokenizer: tokenization.FullTokenizer
+) -> Tuple[run_classifier.InputFeatures, run_classifier.InputFeatures]:
+    train_input_examples, test_input_examples = (
+        ParquetFile(data_filename(dataset_name))
+        .to_pandas()
+        .sample(SAMPLE_SIZE)
+        .apply(create_bert_input_example, axis=1)
+        for dataset_name in DATASET_NAMES
+    )
+
+    train_features, test_features = (
+        run_classifier.convert_examples_to_features(
+            input_examples, LABEL_LIST, MAX_SEQ_LENGTH, tokenizer
+        )
+        for input_examples in (train_input_examples, test_input_examples)
+    )
+
+    return train_features, test_features
+
+
+if __name__ == "__main__":
+    tf.logging.set_verbosity(tf.logging.INFO)
+    tokenizer = create_tokenizer_from_hub_module()
+    print(tokenizer.tokenize("This here's an example of using the BERT tokenizer"))
+
+    create_train_test_features(tokenizer)
